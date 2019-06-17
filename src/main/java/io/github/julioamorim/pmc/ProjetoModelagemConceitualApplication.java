8bbd1ca64820e07d4c5473f1478210790c704ae4
Júlio Amorim <julio.l.amorim00@gmail.com>
2019-06-17 14:06:13 -0300

    Created Project

---
+++ src/main/java/io/github/julioamorim/pmc/ProjetoModelagemConceitualApplication.java
@@ -0,0 +1,13 @@
+package io.github.julioamorim.pmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoModelagemConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoModelagemConceitualApplication.class, args);
+	}
+
+}
